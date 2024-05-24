c8bee03b57905d45d32718b181721c8a66f136d2
Tamara Dakic <tdakic@gmail.com>
2024-05-24 11:57:37 -0700

    clean and comment

--- classes/form/tcq_start_form.php
+++ classes/form/tcq_start_form.php
@@ -1,4 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A part of the form that allows a teacher to start a TCQuiz
+ *
+ * @package   quizaccess_tcquiz
+ * @copyright 2024 Tamara Dakic @Capilano University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 require_once($CFG->libdir."/formslib.php");
 
@@ -13,10 +35,8 @@ class tcq_start_form extends moodleform {
 
         $mform->addElement('submit', 'start_new_session_button', get_string('startnewquiz', 'quizaccess_tcquiz'));
         $mform->addElement('text', 'joincode', '',['placeholder'=> get_string('enterjoincode', 'quizaccess_tcquiz')]);
-        //$mform->addRule('joincode', get_string('err_alphanumeric', 'core_form'), 'regex', '/^[a-zA-Z]+$/', 'client');
         $mform->addRule('joincode', get_string('err_alphanumeric', 'core_form'), 'regex', '/^[a-zA-Z0-9]+$/', 'client');
 
-        //$mform->addRule('joincode', null, 'required', null, 'client'); chose to validate in the validation method
         $mform->setType('joincode', PARAM_TEXT);
 
         //so the page can be redisplayed
@@ -28,7 +48,7 @@ class tcq_start_form extends moodleform {
     // Custom validation of the joincode field
     // Chose not to have the joincode as required, so checking if it is empty here
     // If there is an existing session of the quiz, the joincode is not technically required
-    public function validation($data,$files) {
+    public function validation($data, $files) {
 
       $errors = parent::validation($data,$files);
 
@@ -42,6 +62,7 @@ class tcq_start_form extends moodleform {
 
       if ($tcquiz){ //teacher tried creating a new session,but a session with the same name exists
         $errors['joincode'] = '-'.get_string('sessionexisterror', 'quizaccess_tcquiz');
+        return $errors;
       }
       return $errors;
 
--- classes/form/tcq_student_join_form.php
+++ classes/form/tcq_student_join_form.php
@@ -1,4 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A part of the form that allows a student to join a TCQuiz
+ *
+ * @package   quizaccess_tcquiz
+ * @copyright 2024 Tamara Dakic @Capilano University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 require_once($CFG->libdir."/formslib.php");
 
@@ -8,7 +30,7 @@ class tcq_student_join_form extends moodleform {
 
         $mform = $this->_form;
 
-        //I want the two items in this form to be displayed side-by-side
+        //If I want the two items in this form to be displayed side-by-side
         //$mform->addElement('html', '<style> .fitem {display: inline-block;}</style>');
 
         $mform->addElement('text', 'joincode', '',['placeholder'=> get_string('entercode', 'quizaccess_tcquiz'), 'size'=>25]);
@@ -32,20 +54,23 @@ class tcq_student_join_form extends moodleform {
 
       $errors = parent::validation($data,$files);
 
-    /*  if ($data['joincode'] == ""){
+      if ($data['joincode'] == ""){
         $errors['joincode'] = '-'.get_string('err_required', 'core_form');
         return $errors;
-      }*/
+      }
 
       global $DB;
       $tcquiz = $DB->get_record("quizaccess_tcquiz_session", ['quizid' => $this->_customdata['quizid'],'joincode'=>$data['joincode']]);
 
       if (!$tcquiz){ //no quiz with such joincode
         $errors['joincode'] = '-'.get_string('wrongjoincode', 'quizaccess_tcquiz');
+        return $errors;
       }
-      else if ($tcquiz->status == 0 || $tcquiz->status == 50) { //there is a quiz with such joincode, but it is not running
+      if ($tcquiz->status == 0 || $tcquiz->status == 50) { //there is a quiz with such joincode, but it is not running
         $errors['joincode'] = '-'.get_string('quiznotrunning', 'quizaccess_tcquiz');
+        return $errors;
       }
+
       return $errors;
 
     }
